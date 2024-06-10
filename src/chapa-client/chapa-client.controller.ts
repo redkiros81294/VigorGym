471900bbf3e67310a75eb4e2d0331ddcd14dd4c8
yared <19re1kiros2d94@gmail.com>
2024-06-10 16:02:44 +0300

    2nd front back connection  commit

--- src/chapa-client/chapa-client.controller.ts
+++ src/chapa-client/chapa-client.controller.ts
@@ -1,14 +1,4 @@
-/**
- *  ChapaClientController is a testing controller that verifies that
- *  ChapaModule was generated properly.
- *
- *  You can quickly verify this by running `npm run start:dev`, and then
- *  connecting to `http://localhost:3000` with your browser.  It should return
- *  a custom message like `Hello from ChapaModule`.
- *
- *  Once you begin customizing ChapaModule, you'll probably want
- *  to delete this controller.
- */
+
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { InitializeOptions } from '../chapa-sdk/interfaces/initialize.interface';
 import { VerifyOptions } from '../chapa-sdk/interfaces/verify.interface';
--- src/chapa-client/main.ts
+++ src/chapa-client/main.ts
@@ -1,8 +1,4 @@
-/**
- *  If you're building a standalone npm package hosting a dynamic module, you
- *  should delete this file.  Its only purpose is to bootstrap the app so that
- *  you can run the quick verification test (see chapa-client/chapa-client.module.ts)
- */
+
 import { NestFactory } from '@nestjs/core';
 import { ChapaClientModule } from './chapa-client.module';
 
